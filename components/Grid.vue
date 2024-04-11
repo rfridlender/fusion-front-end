3b0d61adca8c7507071555f233a3d46ca0fc7b3c
Robert Fridlender <robertfridlender@gmail.com>
2024-04-11 01:56:13 -0400

    Add protected layout with Grid + Navbar + Sidebar components

---
+++ components/Grid.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+        <Sidebar />
+        <div class="flex flex-col">
+            <Navbar />
+            <slot />
+        </div>
+    </div>
+</template>
