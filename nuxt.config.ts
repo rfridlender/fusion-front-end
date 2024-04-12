d7dd3b2a8df2f09b925a2357b2e41d3c68eba221
Robert Fridlender <robertfridlender@gmail.com>
2024-04-11 21:41:25 -0400

    Add vee-validate dependency

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,12 @@
 export default defineNuxtConfig({
     devtools: { enabled: true },
     modules: [
+        "@formkit/auto-animate/nuxt",
         "@nuxtjs/color-mode",
         "@nuxtjs/eslint-module",
         "@nuxtjs/tailwindcss",
         "shadcn-nuxt",
+        "@vee-validate/nuxt",
     ],
     colorMode: {
         classSuffix: "",
@@ -16,5 +18,8 @@ export default defineNuxtConfig({
     shadcn: {
         prefix: "",
         componentDir: "./components/ui",
+    },
+    veeValidate: {
+        autoImports: true,
     },
 })
