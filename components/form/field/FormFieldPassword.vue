d151c3ee9bc96a738cd538ef12abdc1a2b64822e
Robert Fridlender <robertfridlender@gmail.com>
2024-04-13 05:17:00 -0400

    Abstract FormFieldPassword component

---
+++ components/form/field/FormFieldPassword.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { Eye, EyeOff } from "lucide-vue-next"
+import type { ComponentFieldBindingObject } from "vee-validate"
+
+const { componentField } = defineProps<{
+    componentField: ComponentFieldBindingObject<any>
+}>()
+
+const isPasswordVisible = useState(componentField.name, () => false)
+</script>
+
+<template>
+    <div class="relative w-full max-w-sm items-center">
+        <Input 
+            :type="!isPasswordVisible ? 'password' : 'text'"
+            placeholder="••••••••" 
+            v-bind="componentField" 
+        />
+        <span 
+            v-auto-animate
+            class="absolute end-0 inset-y-0 flex items-center justify-center px-2"
+        >
+            <Eye 
+                v-if="!isPasswordVisible"
+                class="size-6 transition-all opacity-50 hover:opacity-100 cursor-pointer"
+                @click="isPasswordVisible = true"
+            />
+            <EyeOff
+                v-else
+                v-auto-animate
+                class="size-6 transition-all opacity-50 hover:opacity-100 cursor-pointer"
+                @click="isPasswordVisible = false"
+            />
+        </span>
+    </div>
+</template>
