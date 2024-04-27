b635f616a1b840b0fb3d004e4e31c5c8981a4c63
Robert Fridlender <robertfridlender@gmail.com>
2024-04-27 16:03:47 -0400

    Remove use of axios

--- server/api/address/index.post.ts
+++ server/api/address/index.post.ts
@@ -1,41 +1,20 @@
 import { runAmplifyApi } from "@/server/utils/amplify-utils"
 import { fetchAuthSession } from "aws-amplify/auth/server"
-import axios from "axios"
 
 export default defineEventHandler(async (event) => {
-    try {
-        const { httpApiInvokeUrl } = useRuntimeConfig(event)
-    
-        const session = await runAmplifyApi(event, (contextSpec) => fetchAuthSession(contextSpec))
-        const idToken = session.tokens?.idToken?.toString()
-    
-        const body = await readBody(event)
-    
-        const { data } = await axios({
-            method: "POST",
-            baseURL: httpApiInvokeUrl,
-            url: "/address",
-            headers: { Authorization: `Bearer ${idToken}` },
-            data: body,
-        })
-    
-        return data
-    } catch (error: any) {
-        console.error(error)
+    const { httpApiInvokeUrl } = useRuntimeConfig(event)
 
-        switch (error.name) {
-        case "AxiosError": {
-            throw createError({
-                statusCode: error.response.status,
-                statusMessage: error.response.data.message,
-            })
-        }
-        default: {
-            throw createError({
-                statusCode: 400,
-                statusMessage: error.message,
-            })
-        }
-        }
-    }
+    const session = await runAmplifyApi(event, (contextSpec) => fetchAuthSession(contextSpec))
+    const idToken = session.tokens?.idToken?.toString()
+
+    const body: FormAddress = await readBody(event)
+    
+    const address: Address = await $fetch("/address", {
+        method: "POST",
+        baseURL: httpApiInvokeUrl,
+        headers: { Authorization: `Bearer ${idToken}` },
+        body: body,
+    })
+
+    return address
 })
